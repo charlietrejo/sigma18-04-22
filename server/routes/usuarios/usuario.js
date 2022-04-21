9e93e22d7f1a5a8e6bf1f7acbe4632d7c782e250
Trejo Cruz <jcruzt@sigma-alimentos.com>
2022-04-20 20:20:15 -0500

    Se genero la ruta del metodo det llamada obtenerUsuario que requiere el id usuario y retorna el usuario encontrado

--- server/routes/usuarios/usuario.js
+++ server/routes/usuarios/usuario.js
@@ -1,143 +1,173 @@
 const express = require('express');
 const res = require('express/lib/response');
 const app = express.Router();
-let arrJsnUsuarios = []
+
+app.get('/obtenerUsuario', (req,res) => {
+    const _idUsuario = Number(req.query._idUsuario);
+    if (!_idUsuario) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'No se recibio un identificador de usuario',
+            cont: {
+                _idUsuario
+            }
+        })
+    }
+    const obtenerUsuario = arrJsnUsuarios.find(usuario => usuario._id == _idUsuario);
+    if (!obtenerUsuario) {
+        return res.status(400).json({
+            ok: false,
+            msg: `El usuario con el id: ${_idUsuario} , no se encuentra registrado en la base de datos`,
+            cont: {
+                _idUsuario
+            }
+        })
+    }
+    return res.status(200).json({
+        ok: true,
+        msg: 'Estoy en el segundo get',
+        cont: {
+            _idUsuario
+        }
+    })
+})
+//let arrJsnUsuarios = [{ _id:}]
 // const path = require('path');
 // const rutaDescarga = path.resolve(__dirname, '../../assets/index.html')
-app.get('/', (req, res) =>{
-})
+// app.get('/', (req, res) =>{
+// })
 
-app.post('/', (req, res) =>{
-})
+// app.post('/', (req, res) =>{
+// })
 
-app.delete('/', (req,res) => {
-    const _IdUsuario = parseInt(req.query._IdUsuario);
-    if (!_IdUsuario) {
-        return req.status(400).json({
-           ok: false,
-            msg: 'No se recibio un identificador de usuario', 
-            cont: {
-            _IdUsuario
-            }
-        })
-    }
-    const encontroUsuario = arrJsnUsuarios.find(usuario => usuario._IdUsuario == _IdUsuario)
-    if (!encontroUsuario) {
-        return res.status(400).json({
-            ok: false,
-            msg: `No se encontro un usuario con el Id: ${_IdUsuario} en la base de datos`, 
-            cont: {
-                _IdUsuario
-            }
-        })
-    }
-    const usuarioFiltrado = arrJsnUsuarios.filter(usuario => usuario._IdUsuario != _IdUsuario);
-    arrJsnUsuarios = usuarioFiltrado;
+// app.delete('/', (req,res) => {
+//     const _IdUsuario = parseInt(req.query._IdUsuario);
+//     if (!_IdUsuario) {
+//         return req.status(400).json({
+//            ok: false,
+//             msg: 'No se recibio un identificador de usuario', 
+//             cont: {
+//             _IdUsuario
+//             }
+//         })
+//     }
+    // const encontroUsuario = arrJsnUsuarios.find(usuario => usuario._IdUsuario == _IdUsuario)
+    // if (!encontroUsuario) {
+    //     return res.status(400).json({
+    //         ok: false,
+    //         msg: `No se encontro un usuario con el Id: ${_IdUsuario} en la base de datos`, 
+    //         cont: {
+    //             _IdUsuario
+    //         }
+    //     })
+    // }
+//     const usuarioFiltrado = arrJsnUsuarios.filter(usuario => usuario._IdUsuario != _IdUsuario);
+//     arrJsnUsuarios = usuarioFiltrado;
 
-    return res.status(200).json({
-        ok: false,
-        msg:'Se elimino el usuario de manera exitosa',
-        cont: {
-            encontroUsuario
-        }
-    })
-})
+//     return res.status(200).json({
+//         ok: false,
+//         msg:'Se elimino el usuario de manera exitosa',
+//         cont: {
+//             encontroUsuario
+//         }
+//     })
+// })
 
-app.put('/', (req, res) => {
-    const _IdUsuario = parseInt(req.query._IdUsuario);
-    if (_IdUsuario) {
+// app.put('/', (req, res) => {
+//     const _IdUsuario = parseInt(req.query._IdUsuario);
+//     if (_IdUsuario) {
         
-    }
-}
+//     }
+// }
 
-app.get('/',(req,res) => {
-    const arrUsuarios = arrJsnUsuarios;
-    if (arrUsuarios.length > 0) {
-        return res.status(200).json({
-            ok: true,
-            msg: 'Se recibieron los usuarios de manera exitosa',
-            cont: {
-                arrUsuarios
-            }
-        })
-    }else{
-        return res.status(400).json({
-            ok: false,
-            msg: 'No se encontraron usuarios',
-            cont
-        })
-    }
-   return res.status(200).json({
-        ok:true,
-        msg: 'Se recibieron los usuarios de manera exitosa',
-        cont:{
-            arrUsuarios
-        }
+// app.get('/',(req,res) => {
+//     const arrUsuarios = arrJsnUsuarios;
+//     if (arrUsuarios.length > 0) {
+//         return res.status(200).json({
+//             ok: true,
+//             msg: 'Se recibieron los usuarios de manera exitosa',
+//             cont: {
+//                 arrUsuarios
+//             }
+//         })
+//     }else{
+//         return res.status(400).json({
+//             ok: false,
+//             msg: 'No se encontraron usuarios',
+//             cont
+//         })
+//     }
+//    return res.status(200).json({
+//         ok:true,
+//         msg: 'Se recibieron los usuarios de manera exitosa',
+//         cont:{
+//             arrUsuarios
+//         }
         
-    })
-})
+//     })
+// })
 
-app.post('/',(req,res) => {
+// app.post('/',(req,res) => {
     
-        const body = {
-        strNombre: req.body.strNombre,
-        strApellido: req.body.strApellido,
-        strEmail: req.body.strEmail,
-        _id: Number(req.body._id)
-        }
+//         const body = {
+//         strNombre: req.body.strNombre,
+//         strApellido: req.body.strApellido,
+//         strEmail: req.body.strEmail,
+//         _id: Number(req.body._id)
+//         }
     
-    if (body.strNombre && body.strApellido && body.strEmail && body._id) {
+//     if (body.strNombre && body.strApellido && body.strEmail && body._id) {
         
-        const encontroUsuario = arrJsnUsuarios.find(usuario => usuario._id == body._id || usuario.strEmail == body.strEmail); 
+//         const encontroUsuario = arrJsnUsuarios.find(usuario => usuario._id == body._id || usuario.strEmail == body.strEmail); 
             
-        console.log(encontroUsuario, 'valor');
+//         console.log(encontroUsuario, 'valor');
 
-        arrJsnUsuarios.find(usuario => { console.log(usuario) })
+//         arrJsnUsuarios.find(usuario => { console.log(usuario) })
 
-        arrJsnUsuarios.find(res)
-    arrJsnUsuarios.push(body)
-    res.status(200).json({
-        ok:true,
-        msg: 'Se registro el usuario de manera correcta',
-        cont: {
-            arrJsnUsuarios
-        }
-    })
-}else{
-    res.status(400).json({
-        ok:false,
-        msg: 'No se recibio alguno  todos los valores requeridos',
-        cont: {
-            body
-        }
-    })
-}
-    const encontroUsuario = arrJsnUsuarios.find(usuario => usuario._id == body._id)
+//         arrJsnUsuarios.find(res)
+//     arrJsnUsuarios.push(body)
+//     res.status(200).json({
+//         ok:true,
+//         msg: 'Se registro el usuario de manera correcta',
+//         cont: {
+//             arrJsnUsuarios
+//         }
+//     })
+// }else{
+//     res.status(400).json({
+//         ok:false,
+//         msg: 'No se recibio alguno  todos los valores requeridos',
+//         cont: {
+//             body
+//         }
+//     })
+// }
+//     const encontroUsuario = arrJsnUsuarios.find(usuario => usuario._id == body._id)
     
-    if (encotrousuario) {
-        res.status(400).json({
-            ok: false,
-            msg: 'El usuario ya se encuentra registrado',
-            cont: {
-                encontroUsuario
-            }
-        })
-    }else{
-        arrJsnUsuarios.push
-    }
+//     if (encotrousuario) {
+//         res.status(400).json({
+//             ok: false,
+//             msg: 'El usuario ya se encuentra registrado',
+//             cont: {
+//                 encontroUsuario
+//             }
+//         })
+//     }else{
+//         arrJsnUsuarios.push
+//     }
 
-    arrJsnUsuarios.find(res)
-    arrJsnUsuarios.push(body)
-    res.status(200).json({
-        ok:true,
-        msg: 'Seregistro el usuario de manera correcta',
-        cont: {
-            arrJsnUsuarios
-        }
-    })
-    }
+//     arrJsnUsuarios.find(res)
+//     arrJsnUsuarios.push(body)
+//     res.status(200).json({
+//         ok:true,
+//         msg: 'Seregistro el usuario de manera correcta',
+//         cont: {
+//             arrJsnUsuarios
+//         }
+//     })
+//     }
     
     
-})
+// })
 
 module.exports = app;
